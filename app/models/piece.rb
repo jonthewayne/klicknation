e6464457b53d4fa9fb8bc112c02a2bfa3c1dccad
Jon Shumate <jonthewayne@gmail.com>
2010-11-25 13:45:48 -0800

    specified default image for paperclip. this prevents the webserver and ruby.exe from choking on my windows box - on edge right now missing assets can cause this

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,7 +9,9 @@ class Piece < ActiveRecord::Base
   has_attached_file :image, :styles => { :small => "50x50>", :medium => "100x100>" }, :storage => :s3,
                     :s3_credentials => { :access_key_id => S3[:key], :secret_access_key => S3[:secret] },
                     :bucket => S3[:bucket],
-                    :path => ":class/:attachment/:style/:id.:extension"
+                    :path => ":class/:attachment/:style/:id.:extension",
+                    :default_url => '/images/icons/fugue/question-white.png'
+
 
   #validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
