9512a79b6fa2c56119fd634b6aed25824d18a729
Jon Shumate <jonthewayne@gmail.com>
2010-11-27 11:29:11 -0800

    added validation to make sure attribute numbers are whole positive and 0 or greater

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,9 +1,10 @@
 class Piece < ActiveRecord::Base
   validates_presence_of :name
-  validates_numericality_of :attack, :only_integer => true, :message => "can only be whole number."
-  validates_numericality_of :defense, :only_integer => true, :message => "can only be whole number."
-  validates_numericality_of :movement, :only_integer => true, :message => "can only be whole number."
-  validates_numericality_of :cost, :only_integer => true, :message => "can only be whole number."
+
+  validates_numericality_of :attack, :only_integer => true, :greater_than_or_equal_to => 0, :message => "can only be a whole positive number greater than or equal to 0."
+  validates_numericality_of :defense, :only_integer => true, :greater_than_or_equal_to => 0, :message => "can only be a whole positive number greater than or equal to 0."
+  validates_numericality_of :movement, :only_integer => true, :greater_than_or_equal_to => 0, :message => "can only be a whole positive number greater than or equal to 0."
+  validates_numericality_of :cost, :only_integer => true, :greater_than_or_equal_to => 0, :message => "can only be a whole positive number greater than or equal to 0."
   
   
   has_attached_file :image, :styles => { :small => "50x50>", :medium => "100x100>" }, :storage => :s3,
@@ -13,7 +14,7 @@ class Piece < ActiveRecord::Base
                     :default_url => '/images/icons/fugue/question-white.png'
 
 
-  validates_attachment_presence :image
+  #validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png'] 
   
