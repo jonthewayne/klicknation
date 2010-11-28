529104903aaeeb136d033f46187c35a91422b175
Jon Shumate <jonthewayne@gmail.com>
2010-11-27 22:50:58 -0800

    edited controller to make it show most recent game pieces first by default

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -88,7 +88,7 @@ class PiecesController < ApplicationController
   end
   
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
 
