6721af271c5d037e2b2617fc68d7cbff3783745f
Jon Shumate <jonthewayne@gmail.com>
2010-11-28 00:16:16 -0800

    add ability to close flash message

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,10 @@ $(function() {
     $.getScript(this.href);
     return false;
   });
+  $(".close-bt").live("click", function() {
+    $('#message').remove();
+    return false;
+  });  
   $("#pieces_search input").keyup(function() {
     $.get($("#pieces_search").attr("action"), $("#pieces_search").serialize(), null, "script");
     return false;
