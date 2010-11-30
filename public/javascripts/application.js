8f1f359061cdec92bdf795d1f58f895bab74aec4
Jon Shumate <jonthewayne@gmail.com>
2010-11-30 00:25:41 -0800

    regressed rails.js to version that works with jquery 1.4.2 and copied in jquery-1.4.2.min.js from skin folder

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,20 +2,19 @@ $(function() {
   $("#pieces th a, #pagination a").live("click", function() {
     $.getScript(this.href);
     return false;
-  });
- 
+  }); 
   $("#pieces_search input").keyup(function() {
     $.get($("#pieces_search").attr("action"), $("#pieces_search").serialize(), null, "script");
     return false;
   });
-  $('a.remote-delete').live("click", function() {
+ // $('a.delete-remotely').live("click", function() {
     // we just need to add the key/value pair for the DELETE method
     // as the second argument to the JQuery $.post() call
-    if (confirm("Are you sure?")) {
-      $.post(this.href, {
-        _method: 'delete'
-      }, null, "script");
-    }
-    return false;
-  });  
+   // if (confirm("Are you sure?")) {
+   //   $.post(this.href, {
+   //     _method: 'delete'
+   //   }, null, "script");
+   // }
+   // return false;
+ // });  
 });
--- public/javascripts/rails.js
+++ public/javascripts/rails.js
@@ -1,11 +1,11 @@
 /*
- * jquery-ujs
- *
- * http://github.com/rails/jquery-ujs/blob/master/src/rails.js
- *
- * This rails.js file supports jQuery 1.4.3 and 1.4.4 .
- *
- */ 
+* jquery-ujs
+*
+* http://github.com/rails/jquery-ujs/blob/master/src/rails.js
+*
+* This rails.js file supports jQuery 1.4, 1.4.1 and 1.4.2 .
+*
+*/
 
 jQuery(function ($) {
     var csrf_token = $('meta[name=csrf-token]').attr('content'),
@@ -13,13 +13,13 @@ jQuery(function ($) {
 
     $.fn.extend({
         /**
-         * Triggers a custom event on an element and returns the event result
-         * this is used to get around not being able to ensure callbacks are placed
-         * at the end of the chain.
-         *
-         * TODO: deprecate with jQuery 1.4.2 release, in favor of subscribing to our
-         *       own events and placing ourselves at the end of the chain.
-         */
+* Triggers a custom event on an element and returns the event result
+* this is used to get around not being able to ensure callbacks are placed
+* at the end of the chain.
+*
+* TODO: deprecate with jQuery 1.4.2 release, in favor of subscribing to our
+* own events and placing ourselves at the end of the chain.
+*/
         triggerAndReturn: function (name, data) {
             var event = new $.Event(name);
             this.trigger(event, data);
@@ -28,23 +28,23 @@ jQuery(function ($) {
         },
 
         /**
-         * Handles execution of remote calls. Provides following callbacks:
-         *
-         * - ajax:before   - is execute before the whole thing begings
-         * - ajax:loading  - is executed before firing ajax call
-         * - ajax:success  - is executed when status is success
-         * - ajax:complete - is execute when status is complete
-         * - ajax:failure  - is execute in case of error
-         * - ajax:after    - is execute every single time at the end of ajax call 
-         */
+* Handles execution of remote calls. Provides following callbacks:
+*
+* - ajax:before - is execute before the whole thing begings
+* - ajax:loading - is executed before firing ajax call
+* - ajax:success - is executed when status is success
+* - ajax:complete - is execute when status is complete
+* - ajax:failure - is execute in case of error
+* - ajax:after - is execute every single time at the end of ajax call
+*/
         callRemote: function () {
-            var el      = this,
-                method  = el.attr('method') || el.attr('data-method') || 'GET',
-                url     = el.attr('action') || el.attr('href'),
-                dataType  = el.attr('data-type')  || ($.ajaxSettings && $.ajaxSettings.dataType);
+            var el = this,
+                method = el.attr('method') || el.attr('data-method') || 'GET',
+                url = el.attr('action') || el.attr('href'),
+                dataType = el.attr('data-type') || 'script';
 
             if (url === undefined) {
-                throw "No URL specified for remote call (action or href must be present).";
+              throw "No URL specified for remote call (action or href must be present).";
             } else {
                 if (el.triggerAndReturn('ajax:before')) {
                     var data = el.is('form') ? el.serializeArray() : [];
@@ -74,10 +74,9 @@ jQuery(function ($) {
     });
 
     /**
-     *  confirmation handler
-     */
-
-    $('body').delegate('a[data-confirm], button[data-confirm], input[data-confirm]', 'click.rails', function () {
+* confirmation handler
+*/
+    $('a[data-confirm], button[data-confirm], input[data-confirm]').live('click', function () {
         var el = $(this);
         if (el.triggerAndReturn('confirm')) {
             if (!confirm(el.attr('data-confirm'))) {
@@ -89,27 +88,27 @@ jQuery(function ($) {
 
 
     /**
-     * remote handlers
-     */
-    $('form[data-remote]').live('submit.rails', function (e) {
+* remote handlers
+*/
+    $('form[data-remote]').live('submit', function (e) {
         $(this).callRemote();
         e.preventDefault();
     });
 
-    $('a[data-remote],input[data-remote]').live('click.rails', function (e) {
+    $('a[data-remote],input[data-remote]').live('click', function (e) {
         $(this).callRemote();
         e.preventDefault();
     });
 
-    $('a[data-method]:not([data-remote])').live('click.rails', function (e){
+    $('a[data-method]:not([data-remote])').live('click', function (e){
         var link = $(this),
             href = link.attr('href'),
             method = link.attr('data-method'),
             form = $('<form method="post" action="'+href+'"></form>'),
             metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
 
-        if (csrf_param !== undefined && csrf_token !== undefined) {
-            metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
+        if (csrf_param != null && csrf_token != null) {
+          metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
         }
 
         form.hide()
@@ -121,10 +120,10 @@ jQuery(function ($) {
     });
 
     /**
-     * disable-with handlers
-     */
-    var disable_with_input_selector           = 'input[data-disable-with]',
-        disable_with_form_remote_selector     = 'form[data-remote]:has('       + disable_with_input_selector + ')',
+* disable-with handlers
+*/
+    var disable_with_input_selector = 'input[data-disable-with]',
+        disable_with_form_remote_selector = 'form[data-remote]:has(' + disable_with_input_selector + ')',
         disable_with_form_not_remote_selector = 'form:not([data-remote]):has(' + disable_with_input_selector + ')';
 
     var disable_with_input_function = function () {
@@ -136,10 +135,10 @@ jQuery(function ($) {
         });
     };
 
-    $(disable_with_form_remote_selector).live('ajax:before.rails', disable_with_input_function);
-    $(disable_with_form_not_remote_selector).live('submit.rails', disable_with_input_function);
+    $(disable_with_form_remote_selector).live('ajax:before', disable_with_input_function);
+    $(disable_with_form_not_remote_selector).live('submit', disable_with_input_function);
 
-    $(disable_with_form_remote_selector).live('ajax:complete.rails', function () {
+    $(disable_with_form_remote_selector).live('ajax:complete', function () {
         $(this).find(disable_with_input_selector).each(function () {
             var input = $(this);
             input.removeAttr('disabled')
@@ -148,9 +147,8 @@ jQuery(function ($) {
     });
 
     var jqueryVersion = $().jquery;
-
-    if ( (jqueryVersion === '1.4') || (jqueryVersion === '1.4.1') || (jqueryVersion === '1.4.2') ){
-        alert('This rails.js does not support the jQuery version you are using. Please read documentation.');
+    if (!( (jqueryVersion === '1.4') || (jqueryVersion === '1.4.1') || (jqueryVersion === '1.4.2') )){
+      alert('This rails.js does not support the jQuery version you are using. Please read documentation.')
     }
 
 });
