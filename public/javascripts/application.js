d79b31c4aab8228494b5beec180cbf9ec5ba41bf
Jon Shumate <jonthewayne@gmail.com>
2010-11-30 01:13:16 -0800

    removed a bit of extraneous js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,14 +7,4 @@ $(function() {
     $.get($("#pieces_search").attr("action"), $("#pieces_search").serialize(), null, "script");
     return false;
   });
- // $('a.delete-remotely').live("click", function() {
-    // we just need to add the key/value pair for the DELETE method
-    // as the second argument to the JQuery $.post() call
-   // if (confirm("Are you sure?")) {
-   //   $.post(this.href, {
-   //     _method: 'delete'
-   //   }, null, "script");
-   // }
-   // return false;
- // });  
 });
