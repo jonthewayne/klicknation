ddda90cdd25e86982fed6f2dc93e2be698360185
Jon Shumate <jonthewayne@gmail.com>
2011-01-24 15:44:02 -0800

    added Item model, controller, and views. Updated login screen, am working on Item admin

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,22 @@
 class ItemsController < ApplicationController
+  load_and_authorize_resource  
+  helper_method :sort_column, :sort_direction
+  
   # GET /items
   # GET /items.xml
   def index
-    @items = Item.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @items }
-    end
+    get_items
   end
 
   # GET /items/1
   # GET /items/1.xml
   def show
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @item }
-    end
+    redirect_to(edit_item_url(@item))
   end
 
   # GET /items/new
   # GET /items/new.xml
   def new
-    @item = Item.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @item }
@@ -34,17 +25,15 @@ class ItemsController < ApplicationController
 
   # GET /items/1/edit
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   # POST /items
   # POST /items.xml
   def create
-    @item = Item.new(params[:item])
-
     respond_to do |format|
       if @item.save
-        format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
+        format.html { redirect_to(edit_item_url(@item), :notice => 'Item was successfully created.') }
         format.xml  { render :xml => @item, :status => :created, :location => @item }
       else
         format.html { render :action => "new" }
@@ -56,11 +45,9 @@ class ItemsController < ApplicationController
   # PUT /items/1
   # PUT /items/1.xml
   def update
-    @item = Item.find(params[:id])
-
     respond_to do |format|
       if @item.update_attributes(params[:item])
-        format.html { redirect_to(@item, :notice => 'Item was successfully updated.') }
+        format.html { redirect_to(edit_item_url(@item), :notice => 'Item was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -72,12 +59,45 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   # DELETE /items/1.xml
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(items_url) }
-      format.xml  { head :ok }
+    get_items
+    
+    render 'items/index'    
+  end
+
+  private
+  
+  def sort_column
+    Item.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
+  
+  def items_search
+    # when called from destroy method, @admin_tool_users isn't created by cancan method, so create it.
+    if @items    
+      @items = @items.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])          
+    else
+      @items = Item.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])                
     end
   end
+  
+  def get_items
+    items_search
+    
+    if @items.empty? && (params[:page].to_i > 1)
+      # if @items is empty and we're specifying greater than page 1, in most cases we need the page just before
+      params[:page] = (params[:page].to_i - 1)
+      items_search
+      # if @items is still empty, we should just do the query to get the last available page
+      if @items.empty?      
+        params[:page] = @items.total_pages
+        items_search
+      end
+    end    
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,73 @@
 class Item < ActiveRecord::Base
+  # items has type column not used for single table inheritance, so change sti column
+  self.inheritance_column = :item_type
+  
+  before_validation :set_defaults
+  
+  validates :name, :presence => true, :length => {:minimum => 1, :maximum => 60}
+                    
+  validates :description, :presence => true
+                    
+  validates :type, :presence => true, :inclusion => { :in => %w(0 1 2 3 4) }, :message => "%{value} is not a valid item type"
+                    
+  validates :price, :presence => true, :numericality => true
+                    
+  validates :currency_type, :presence => true, :inclusion => { :in => %w(0 1) }
+                    
+  validates :price, :presence => true, :numericality => true
+                    
+  validates :attack, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 6}
+                    
+  validates :defense, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 5}
+                    
+  validates :agility, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 6}
+                    
+  validates :num_available, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 6}
+                    
+  validates :available_on,  :presence => true
+                    
+  validates :available_until,  :presence => true
+                    
+  validates :level, :presence => true, :numericality => { :only_integer => true }
+                    
+  validates :ability_element_id, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 6}
+                    
+  validates :rarity, :presence => true, :inclusion => { :in => %w(0 1 2) }, :message => "%{value} is not a valid rarity type"
+                    
+  validates :photo, :presence => true, :length => {:minimum => 1, :maximum => 200}
+  
+  validates :swf, :length => {:minimum => 1, :maximum => 255}  
+                    
+  validates :item_category_id, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 3}
+                    
+  validates :city, :presence => true, :numericality => { :only_integer => true }
+                    
+  validates :sort, :presence => true, :numericality => { :only_integer => true }, :length => {:minimum => 1, :maximum => 5}
+                    
+  validates :class, :presence => true, :inclusion => { :in => %w(0 1 2 3) }, :message => "%{value} is not a valid class type"
+                    
+  validates :force_show_card, :inclusion => { :in => [true, false] }
+                    
+  validates :i_can_has, :inclusion => { :in => [true, false] }
+                    
+  validates :sell_isolated, :inclusion => { :in => [true, false] }
+                    
+  validates :apply_discount, :inclusion => { :in => [true, false] }
+  
+  
+
+  def self.search(search)
+    if search
+      where('name LIKE ?', "%#{search}%") 
+    else
+      scoped
+    end
+  end
+  
+  protected
+  def set_defaults
+    # app id is always 1
+    self.app_id = 1
+    self.sort = 0 # default if not a merit ability
+  end  
 end
