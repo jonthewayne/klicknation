d43beffbd5b301d3c503f0afafda6016960b0d6e
Jon Shumate <jonthewayne@gmail.com>
2011-02-12 03:29:52 -0800

    ignored Thumbs.db that windows adds. added to will_paginate calls so that index page is used in pagination after an ajaxed delete call

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Klicknation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pieces#index"
+  root :to => "items#index"
 
   # See how all your routes lay out with "rake routes"
 
